e0d2042ffb92154de27b5cf36a6ea3fdd4ad5237
yoogodong <yoogodong@gmail.com>
2019-11-17 18:57:06 +0800

    去 lombok

--- src/main/java/api/controller/OrderController.java
+++ src/main/java/api/controller/OrderController.java
@@ -1,7 +1,6 @@
 package api.controller;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -55,7 +54,6 @@ public class OrderController {
     /*
      * 代表应答对象
      * */
-    @Data
     public class OrderOut {
         private double totalPrice;
         @JsonFormat(pattern = "yyyy-MM-dd hh-mm-ss")
@@ -73,9 +71,69 @@ public class OrderController {
             orderItemList.add(orderItem);
             return this;
         }
+
+        public double getTotalPrice() {
+            return this.totalPrice;
+        }
+
+        public void setTotalPrice(double totalPrice) {
+            this.totalPrice = totalPrice;
+        }
+
+        public LocalDateTime getCreatedTime() {
+            return this.createdTime;
+        }
+
+        public void setCreatedTime(LocalDateTime createdTime) {
+            this.createdTime = createdTime;
+        }
+
+        public List<OrderItem> getOrderItemList() {
+            return this.orderItemList;
+        }
+
+        public void setOrderItemList(List<OrderItem> orderItemList) {
+            this.orderItemList = orderItemList;
+        }
+
+        public boolean equals(final Object o) {
+            if (o == this) return true;
+            if (!(o instanceof OrderOut)) return false;
+            final OrderOut other = (OrderOut) o;
+            if (!other.canEqual((Object) this)) return false;
+            if (Double.compare(this.getTotalPrice(), other.getTotalPrice()) != 0) return false;
+            final Object this$createdTime = this.getCreatedTime();
+            final Object other$createdTime = other.getCreatedTime();
+            if (this$createdTime == null ? other$createdTime != null : !this$createdTime.equals(other$createdTime))
+                return false;
+            final Object this$orderItemList = this.getOrderItemList();
+            final Object other$orderItemList = other.getOrderItemList();
+            if (this$orderItemList == null ? other$orderItemList != null : !this$orderItemList.equals(other$orderItemList))
+                return false;
+            return true;
+        }
+
+        protected boolean canEqual(final Object other) {
+            return other instanceof OrderOut;
+        }
+
+        public int hashCode() {
+            final int PRIME = 59;
+            int result = 1;
+            final long $totalPrice = Double.doubleToLongBits(this.getTotalPrice());
+            result = result * PRIME + (int) ($totalPrice >>> 32 ^ $totalPrice);
+            final Object $createdTime = this.getCreatedTime();
+            result = result * PRIME + ($createdTime == null ? 43 : $createdTime.hashCode());
+            final Object $orderItemList = this.getOrderItemList();
+            result = result * PRIME + ($orderItemList == null ? 43 : $orderItemList.hashCode());
+            return result;
+        }
+
+        public String toString() {
+            return "OrderController.OrderOut(totalPrice=" + this.getTotalPrice() + ", createdTime=" + this.getCreatedTime() + ", orderItemList=" + this.getOrderItemList() + ")";
+        }
     }
 
-    @Data
     public class OrderItem {
         int quantity;
         String name;
@@ -83,6 +141,51 @@ public class OrderController {
         public OrderItem(int quantity, String name) {
             this.quantity = quantity;
             this.name = name;
+        }
+
+        public int getQuantity() {
+            return this.quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public boolean equals(final Object o) {
+            if (o == this) return true;
+            if (!(o instanceof OrderItem)) return false;
+            final OrderItem other = (OrderItem) o;
+            if (!other.canEqual((Object) this)) return false;
+            if (this.getQuantity() != other.getQuantity()) return false;
+            final Object this$name = this.getName();
+            final Object other$name = other.getName();
+            if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+            return true;
+        }
+
+        protected boolean canEqual(final Object other) {
+            return other instanceof OrderItem;
+        }
+
+        public int hashCode() {
+            final int PRIME = 59;
+            int result = 1;
+            result = result * PRIME + this.getQuantity();
+            final Object $name = this.getName();
+            result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+            return result;
+        }
+
+        public String toString() {
+            return "OrderController.OrderItem(quantity=" + this.getQuantity() + ", name=" + this.getName() + ")";
         }
     }
 }
