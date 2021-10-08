3f2a99e344f1843fc0991550c718f8d2e25ec107
yoogodong <yoogodong@gmail.com>
2021-10-08 17:38:43 +0800

    添加培训时需要的注释

--- src/main/java/ut/second/Accumulator.java
+++ src/main/java/ut/second/Accumulator.java
@@ -1,5 +1,14 @@
 package ut.second;
 
+/**
+ * 1，分析应该有几个测试用例
+ * 2，首先要保证逻辑分支的覆盖， 然后要考虑边界值
+ * 3，是否该测试所有的方法？
+ * 4，是否该测试私有方法？
+ * 5，在IDE中获得测试覆盖率
+ * 6，在命令行通过 mvn jacoco:report 获得测试覆盖率（已经在 pom 中配好 jacoco-plugin)
+ * 7，测试覆盖率的几种指标：行、分支、方法、类、指令
+ */
 public class Accumulator {
 
     public int accumulate(int input) throws NumberOutOfBoundException {
