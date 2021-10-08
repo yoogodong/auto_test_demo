6b07c7f81888987e8b2ad178bc87cd225780022b
yoogodong <yoogodong@gmail.com>
2021-10-08 14:42:12 +0800

    介绍测试的基本结构

---
+++ src/test/java/ut/basic/StartTest.java
@@ -0,0 +1,32 @@
+package ut.basic;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class StartTest {
+
+    @Test
+    public void name() {
+        //given
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+
+        String s = list.get(0);//when
+
+        assertEquals("2", s);//then
+    }
+
+    @Test
+    public void bb() {
+        //given
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+
+        String s = list.get(0);//when
+
+        assertEquals("1", s);//then
+    }
+}
