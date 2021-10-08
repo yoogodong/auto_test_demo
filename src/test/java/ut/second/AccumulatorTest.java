68f5dfe153d29864e196cc2d003a1d1f95f5c8aa
yoogodong <yoogodong@gmail.com>
2021-10-08 17:38:16 +0800

    重构测试方法的命名，添加断言注释

--- src/test/java/ut/second/AccumulatorTest.java
+++ src/test/java/ut/second/AccumulatorTest.java
@@ -8,7 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
- * 1.测试类和被测类共享包名
+ * 1，测试类和被测类共享包名
+ * 2，一个被测方法有多个测试用例，这里将每个用例都对应了一个测试方法，这种方式更清晰，所以推荐使用
+ * 3，使用 assertThrows 来断言方法应该抛出异常
+ *
  */
 public class AccumulatorTest {
 
@@ -20,29 +23,28 @@ public class AccumulatorTest {
     }
 
     @Test
-    public void accumulate_should_return_0_when_input_0() throws NumberOutOfBoundException {
+    public void testAccumulate_0() throws NumberOutOfBoundException {
         int result = accumulator.accumulate(0);
-        assertEquals(0,result);
+        assertEquals(0,result,"输入为0的累加结果不对");
     }
 
     @Test
-    public void accumulate_should_return_50050_when_input_1000() throws NumberOutOfBoundException {
+    public void testAccumulate_1000() throws NumberOutOfBoundException {
         int result = accumulator.accumulate(1000);
-        assertEquals(500500,result);
+        assertEquals(500500,result,"输入为1000的累加结果不对");
     }
 
     @Test
-    public void accumulate_should_throw_when_input_is_1001() throws NumberOutOfBoundException {
+    public void testAccumulate_1001() throws NumberOutOfBoundException {
         Assertions.assertThrows(NumberOutOfBoundException.class,()->{
             accumulator.accumulate(1001);
-        });
+        },"输入为1001的累加结果不对");
     }
 
     @Test
-    public void accumulate_should_throw_when_input_is_negative1() throws NumberOutOfBoundException {
+    public void testAccumulate_Minus1() throws NumberOutOfBoundException {
         Assertions.assertThrows(NumberOutOfBoundException.class,()->{
             accumulator.accumulate(-1);
-        });
+        },"输入为-1的累加结果不对");
     }
-
 }
