83f3e48995cc67489e0baabec6616404f80c6e9a
yoogodong <yoogodong@gmail.com>
2021-10-13 12:57:33 +0800

    添加 assertThat 示例

--- src/test/java/ut1/StringBuilderTest.java
+++ src/test/java/ut1/StringBuilderTest.java
@@ -3,20 +3,26 @@ package ut1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * 通过如何通过 Unit Test 来测试 SpringBuilder 的部分API，以明白UT的基本用法
- * 1，测试类名 = 被测类 + Test
- * 2，测试方法命名要表明测试目的， 可以有任意个测试方法，但要围绕一个被测类
- * 3，每个测试都至少包含一个 given-when-then 结构
- * 4, given 是给定条件，when 是被测试目标，then 是结果断言
- * 5，测试框架junit,会调用所有的 @Test 标注的方法， 不同的测试方法没有确定的执行顺序
- * 6，常用的标注，有 @Test、@BeforeEach、 @AfterEach @DisplayName
- * 7, 常用的断言方法，还有 assertTrue、assertFalse、assertThat
- * 8, 需要多个断言的时候，考虑是否可以使用 assertThat, assertThat 可以简化断言的表达 @Todo
- * 9，运行测试，可以在 IDE 中，也可以通过 maven 命令（mvn test）
- * 10, 这里的必要依赖是 junit5, 这里是通过引用 spring-boot-starter-test 来包含这个依赖的
+ * 测试类和测试方法
+ *      1，测试类名 = 被测类 + Test
+ *      2，测试方法命名要表明测试目的， 可以有任意个测试方法，但要围绕一个被测类
+ *      3，每个测试都至少包含一个 given-when-then 结构
+ *      4, given 是给定条件，when 是被测试目标，then 是结果断言
+ *
+ * 测试框架和断言库
+ *      1，测试框架junit,会调用所有的 @Test 标注的方法， 不同的测试方法没有确定的执行顺序
+ *      2，常用的标注，有 @Test、@BeforeEach、 @AfterEach @DisplayName
+ *      3, 常用的断言方法，还有 assertTrue、assertFalse；
+ *      4, 同时需要对一个对象进行多个断言的时候，考虑使用第三方的断言库简化断言的表达
+ *      5, AssertJ 是目前最流行的断言库，assertThat() 是其提供的核心方法
+ *      5，这里是通过引用 spring-boot-starter-test 来引入所需这两个依赖
+ *
+ * 运行测试，可以在 IDE 中，也可以通过 maven 命令（mvn test）
  */
 public class StringBuilderTest {
 
@@ -44,5 +50,14 @@ public class StringBuilderTest {
 
         builder.append(2345);
         assertEquals(5,builder.length(),"长度不对");
+    }
+
+    @Test
+    void show_assertThat() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("12345");
+
+        assertThat(builder).containsOnlyDigits().endsWith("45");
     }
 }
