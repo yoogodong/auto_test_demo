9d13642debf0010039935eefa60f223fecbf6902
yoogodong <yoogodong@gmail.com>
2021-10-14 13:09:17 +0800

    明确测试目标

--- src/test/java/component/interfaces/OrderController1CT.java
+++ src/test/java/component/interfaces/OrderController1CT.java
@@ -12,11 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- *
+ * 演示组件测试的基本结构
+ * @SpringBootTest Spring 管理测试实例
+ * @Transactional 可以保证每个测试后都会回滚事务
  */
 @SpringBootTest(classes = {Application.class})
 @Transactional
-class OrderControllerCT{
+class OrderController1CT {
 
     @Autowired
     OrderController controller;
@@ -25,6 +27,9 @@ class OrderControllerCT{
 
     private Order saved;
 
+    /**
+     * 测试运行前清理相关的表，以保证测试独立不受影响
+     */
     @BeforeEach
     void setUp() {
         repository.deleteAllInBatch();
